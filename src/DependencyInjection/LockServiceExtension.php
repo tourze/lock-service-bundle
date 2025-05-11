500a56d93c443c559037dee5152a64be82a22912
github-ci <you@example.com>
2025-05-11 07:30:37 +0000

    feat(lock-service-bundle): 添加 Redis 依赖并配置预加载

--- src/DependencyInjection/LockServiceExtension.php
+++ src/DependencyInjection/LockServiceExtension.php
@@ -5,9 +5,10 @@ namespace Tourze\LockServiceBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class LockServiceExtension extends Extension
+class LockServiceExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -16,5 +17,59 @@ class LockServiceExtension extends Extension
             new FileLocator(__DIR__ . '/../Resources/config')
         );
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependRedis($container);
+        $this->prependDoctrine($container);
+    }
+
+    /**
+     * Redis配置
+     */
+    private function prependRedis(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('snc_redis', [
+            'clients' => [
+                'lock' => [
+                    'type' => 'phpredis',
+                    'alias' => 'lock',
+                    'dsn' => $_ENV['LOCK_REDIS_URL'] ?? $_ENV['REDIS_URL'] ?? 'redis://127.0.0.1:6379',
+                    'logging' => false,
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * DBAL配置
+     */
+    private function prependDoctrine(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('doctrine', [
+            'dbal' => [
+                'connections' => [
+                    'lock' => [
+                        'url' => $_ENV['LOCK_DATABASE_URL'] ?? $_ENV['DATABASE_URL'] ?? 'sqlite:///:memory:',
+
+                        // When true, queries are logged to a "doctrine" monolog channel
+                        'logging' => $container->resolveEnvPlaceholders("%kernel.debug%"),
+                        'profiling' => $container->resolveEnvPlaceholders("%kernel.debug%"),
+
+                        // When true, profiling also collects a backtrace for each query
+                        'profiling_collect_backtrace' => true,
+
+                        //  When true, profiling also collects schema errors for each query
+                        'profiling_collect_schema_errors' => true,
+
+                        'use_savepoints' => true,
+                        'mapping_types' => [
+                            'enum' => 'string',
+                        ],
+                    ],
+                ],
+            ],
+        ]);
     }
 }
--- src/LockServiceBundle.php
+++ src/LockServiceBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\LockServiceBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class LockServiceBundle extends Bundle
+class LockServiceBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+        ];
+    }
 }
